77ed86f8645ea0660374159e9e3cec9c03a0128b
Ezra Wu <wushijie5@gmail.com>
2013-09-01 16:35:06 +0800

    updated

--- Html.java
+++ Html.java
@@ -1,6 +1,15 @@
 package com.example.newsreader;
 
 
+import android.util.Log;
+
+import org.apache.http.util.ByteArrayBuffer;
+import org.apache.http.util.EncodingUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
+
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.net.URL;
@@ -9,14 +18,6 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.http.util.ByteArrayBuffer;
-import org.apache.http.util.EncodingUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.safety.Whitelist;
-
-import android.util.Log;
-
 public class Html {
     private String url_yes;
 
@@ -24,6 +25,8 @@ public class Html {
 
     Html(String a){url_yes=a;Sources=getHtmlString();}
     Html(String a,int i){url_yes=a;}
+    //for utf-8
+    Html(String a,String t){url_yes=a;Sources=getHtmlStringForUTF8();}
 
     public String getHtmlString() {
         try {
@@ -45,56 +48,108 @@ public class Html {
         }
     }
 
-    //for home page
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-	public ArrayList getAllUrl(){
+    public String getHtmlStringForUTF8() {
+        try {
+            URL url = new URL(url_yes);
+            URLConnection ucon = url.openConnection();
+            InputStream instr = ucon.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(instr);
+            ByteArrayBuffer baf = new ByteArrayBuffer(500);
+            int current = 0;
+            while ((current = bis.read()) != -1) {
+                baf.append((byte) current);
+            }
+            //对应网页编码
+            Sources=EncodingUtils.getString(baf.toByteArray(), "UTF-8");
+            Log.v("test!!!","finishgettingsource");
+            return EncodingUtils.getString(baf.toByteArray(), "UTF-8");
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public ArrayList getAllUrlFromSysumcPart1(){
         Pattern new_col = Pattern.compile("<table width=\"100%\" cellpadding=\"3\">(.*?)</table>");
         Matcher matchedCol = new_col.matcher(Sources);
-        String title=new String();
+        ArrayList url=new ArrayList();
+        while (matchedCol.find()){
+            url.add(matchedCol.group(1));
+        }
+        return url;
+    }
+
+    public ArrayList getAllUrlFromSysumcPart2(String temp){
+        Pattern news = Pattern.compile("<a.*?href=\"(.*?)\"");
+        Matcher matchedTitle = news.matcher(temp);
+        ArrayList url=new ArrayList();
+        int count=0;
+        //get all of them by while
+        while (matchedTitle.find()){
+            url.add(url_yes+matchedTitle.group(1));
+            count++;
+            Log.v("test!!!", url_yes+matchedTitle.group(1));
+        }
+        Log.v("test!!!",String.valueOf(count));
+        return url;
+    }
+
+    public ArrayList getAllUrlFromJwc(){
+        Pattern new_col = Pattern.compile("<li><a href=\"(.*?)\" title");
+        Matcher matchedCol = new_col.matcher(Sources);
         ArrayList url=new ArrayList();
         int count=0;
         while (matchedCol.find()){
-            title = matchedCol.group(1);
-            Pattern news = Pattern.compile("<a.*?href=\"(.*?)\"");
-            Matcher matchedTitle = news.matcher(title);
-            //get all of them by while
-            while (matchedTitle.find()){
-                url.add(url_yes+matchedTitle.group(1));
-                count++;
-                Log.v("test!!!", url_yes+matchedTitle.group(1));
-            }
+            url.add(matchedCol.group(1));
+            count++;
         }
         Log.v("test!!!", String.valueOf(count));
         return url;
     }
 
-    public String getCol(){
-        String Col=new String();
-        //xueshengyuandi
-        Pattern patterncol_student=Pattern.compile("(.*ColumnNo=NA05.*)");
-        //jiaoxuejingwei
-        Pattern patterncol_teaching=Pattern.compile("(.*ColumnNo=NA02.*)");
-        //xuesuzixun
-        Pattern patterncol_learning=Pattern.compile("(.*ColumnNo=NA03.*)");
-        //xueyuandongtai
-        Pattern patterncol_activity=Pattern.compile("(.*ColumnNo=NA01.*)");
-        Matcher matchactivity=patterncol_activity.matcher(url_yes);
-        Matcher matchlearning=patterncol_learning.matcher(url_yes);
-        Matcher matchstudent=patterncol_student.matcher(url_yes);
-        Matcher matchteaching=patterncol_teaching.matcher(url_yes);
-        if (matchactivity.find()){
-            Col="学院动态";
+    public ArrayList getAllUrlFromXscPart1(){
+        Pattern new_col = Pattern.compile("<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">([\\s\\S]*?)</table>");
+        Matcher matchedCol = new_col.matcher(Sources);
+        ArrayList url=new ArrayList();
+        while (matchedCol.find()){
+            url.add(matchedCol.group(1));
         }
-        if (matchlearning.find()){
-            Col="学术资讯";
+        return url;
+    }
+
+    public ArrayList getAllUrlFromXscPart2(String temp){
+        Pattern new_col = Pattern.compile("  <a href=\"(.*?)\"");
+        Matcher matchedCol = new_col.matcher(temp);
+        ArrayList url=new ArrayList();
+        int count=0;
+        while (matchedCol.find()){
+            url.add("http://xsc2000.sysu.edu.cn/"+matchedCol.group(1));
+            count++;
         }
-        if (matchstudent.find()){
-            Col="学生园地";
+        Log.v("test!!!",String.valueOf(count));
+        return url;
+    }
+
+    public ArrayList getAllItem(){
+        Pattern item=Pattern.compile("<item>([\\s\\S]*?)</item>");
+        Matcher matchedItem=item.matcher(Sources);
+        ArrayList items=new ArrayList();
+        int count=0;
+        while (matchedItem.find()){
+            items.add(matchedItem.group(1));
+            count++;
         }
-        if (matchteaching.find()){
-            Col="教学经纬";
+        Log.v("test???", String.valueOf(count));
+        return items;
+    }
+
+    public String getRSSTitle(){
+        String title="cant get it";
+        Pattern item=Pattern.compile("<title>(.*?)</title>");
+        Matcher matchedTitle=item.matcher(Sources);
+        if (matchedTitle.find()){
+            title=matchedTitle.group(1);
         }
-        return Col;
+        return title;
     }
 
     public String getTime(){
@@ -108,8 +163,40 @@ public class Html {
         return Time;
     }
 
+    public String getTimeForXsc(){
+        String Time=new String("cant get it");
+        //识别4个汉字。。。
+        Pattern news = Pattern.compile("&nbsp;[\\u4E00-\\u9FA5]{2}：(.*?) &nbsp;");
+        Matcher matchedNews = news.matcher(Sources);
+        if (matchedNews.find()){
+            Time=matchedNews.group(1);
+        }
+        return Time;
+    }
+
+    public String getTimeForJwc(){
+        String Time= "cant get it";
+        //识别4个汉字。。。
+        Pattern news = Pattern.compile("<div class=\"art_property\">.*?[\\u4E00-\\u9FA5]{4}：(.*?)　<a.*?</div>");
+        Matcher matchedNews = news.matcher(Sources);
+        if (matchedNews.find()){
+            Time=matchedNews.group(1);
+        }
+        return Time;
+    }
+
+    public String getTime(String item){
+        String Time= "cant get it";
+        Pattern news = Pattern.compile("<pubDate>(.*?)</pubDate>");
+        Matcher matchedNews = news.matcher(item);
+        if (matchedNews.find()){
+            Time=matchedNews.group(1);
+        }
+        return Time;
+    }
+
     public String getTitle(){
-        String Title=new String("cant get it");
+        String Title="cant get it";
         Pattern news = Pattern.compile("<font size='2'>(.*?)</font>");
         Matcher matchedNews = news.matcher(Sources);
         if (matchedNews.find()){
@@ -118,9 +205,77 @@ public class Html {
         return Title;
     }
 
+    public String getTitleForXsc(){
+        String Title="cant get it";
+        Pattern news = Pattern.compile("<td height=\"50\" colspan=\"2\" align=\"center\" class=\"tit\">(.*?)</td>");
+        Matcher matchedNews = news.matcher(Sources);
+        if (matchedNews.find()){
+            Title=matchedNews.group(1);
+        }
+        return Title;
+    }
+
+    public String getTitleForJwc(){
+        String Title="客户端IP地址访问受限！";
+        Pattern news = Pattern.compile("<h1 class=\"art_title o_h\">(.*?)</h1>");
+        Matcher matchedNews = news.matcher(Sources);
+        if (matchedNews.find()){
+            Title=matchedNews.group(1);
+        }
+        return Title;
+    }
+
+    public String getTitle(String item){
+        String Title="cant get it";
+        Pattern news = Pattern.compile("<title>(.*?)</title>");
+        Matcher matchedNews = news.matcher(item);
+        if (matchedNews.find()){
+            Title=matchedNews.group(1);
+        }
+        return Title;
+    }
+
+    public String getURL(String item){
+        String URL="cant get it";
+        Pattern news = Pattern.compile("<link>(.*?)</link>");
+        Matcher matchedNews = news.matcher(item);
+        if (matchedNews.find()){
+            URL=matchedNews.group(1);
+        }
+        return URL;
+    }
+
+    public String getDescription(String item){
+        String Description="cant get it";
+        Pattern news = Pattern.compile("<description><!\\[CDATA\\[([\\s\\S]*?)\\]\\]></description>");
+        Matcher matchedNews = news.matcher(item);
+        if (matchedNews.find()){
+            Description=matchedNews.group(1);
+        }
+        return Description;
+    }
+
+    public String getContent(String item){
+        String tempContent="cant get it";
+        String Content= "";
+        Pattern news = Pattern.compile("<content:encoded>([\\s\\S]*?)</content:encoded>");
+        Matcher matchedNews = news.matcher(item);
+        if (matchedNews.find()){
+            tempContent=matchedNews.group(1);
+            Pattern temp=Pattern.compile("<!\\[CDATA\\[([\\s\\S]*?)\\]\\]>");
+            Matcher matched=temp.matcher(tempContent);
+            while (matched.find()){
+                Content+=matched.group(1);
+            }
+        }
+
+        return Content;
+    }
+
+
     public String getContent(){
-        String Content=new String("cant get it");
-        String finalContent=new String("cant get it");
+        String Content="cant get it";
+        String finalContent="cant get it";
         Pattern news = Pattern.compile("(<table\\s*width=\"100%\"\\s*align=\"center\">[\\s\\S]*?</table>)");
         Matcher matchedNews = news.matcher(Sources);
         if (matchedNews.find()){
@@ -131,26 +286,67 @@ public class Html {
                 finalContent=delNews.replaceAll("");
             }
         }
-                
+
         Document doc = null;
-    	doc = Jsoup.parse(finalContent);
-    	
+        doc = Jsoup.parse(finalContent);
+
         doc.select("tr").first().remove();
-        doc.select("tr").first().remove(); 
-        
-       return doc.html();
+        doc.select("tr").first().remove();
+
+        return doc.html();
+    }
+
+    public String getContentForJwc(){
+        String Content=new String("cant get it");
+        Document doc=Jsoup.parse(Sources);
+        Elements test=doc.getElementsByClass("content");
+        Content=test.toString();
+        return Content;
+    }
+
+    public String getContentForXsc(){
+        String Content="cant get it";
+        Pattern news = Pattern.compile("<td colspan=\"2\">([\\s\\S]*?)</td>");
+        Matcher matchedNews = news.matcher(Sources);
+        if (matchedNews.find()){
+            Content=matchedNews.group(1);
+        }
+        return Content;
     }
 
     //提取content
     public String parseHtml(){
-    	String finalContent = getContent();
-    	
-    	String parsedContent = "failed";
-    	finalContent = "<html>"+ finalContent + "</html>";
-    	parsedContent = Jsoup.clean(finalContent, Whitelist.relaxed());
-    	Document doc = null;
-    	doc = Jsoup.parse(finalContent);
-    	return doc.text();
+        String finalContent = getContent();
+
+        String parsedContent = "failed";
+        finalContent = "<html>"+ finalContent + "</html>";
+        parsedContent = Jsoup.clean(finalContent, Whitelist.relaxed());
+        Document doc = null;
+        doc = Jsoup.parse(finalContent);
+        return doc.text();
     }
-    
+
+    //提取content
+    public String parseHtmlForJwc(){
+        String finalContent = getContentForJwc();
+
+        String parsedContent = "failed";
+        finalContent = "<html>"+ finalContent + "</html>";
+        parsedContent = Jsoup.clean(finalContent, Whitelist.relaxed());
+        Document doc = null;
+        doc = Jsoup.parse(finalContent);
+        return doc.text();
+    }
+
+    public String parseHtmlForXsc(){
+        String finalContent = getContentForXsc();
+
+        String parsedContent = "failed";
+        finalContent = "<html>"+ finalContent + "</html>";
+        parsedContent = Jsoup.clean(finalContent, Whitelist.relaxed());
+        Document doc = null;
+        doc = Jsoup.parse(finalContent);
+        return doc.text();
+    }
+
 }
